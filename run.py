9c742eaa57aaf1065e03a46c88cfc57196270a9f
jkingportfolio <mycodejourney3@gmail.com>
2022-07-31 12:50:16 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -5,12 +5,13 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
@@ -33,6 +34,7 @@ def validate_data(values):
     """
     try:
         if len(values) != 6:
+            [int(value) for value in values]
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
